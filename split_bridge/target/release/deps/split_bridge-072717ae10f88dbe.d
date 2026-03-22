c4b7836496a7d5a0fee0458d8d6afa6df678d714
fadi-agg <fadiaggoun96@gmail.com>
2026-03-22 12:47:12 +0100

    Delete split_bridge directory

--- split_bridge/target/release/deps/split_bridge-072717ae10f88dbe.d
+++
@@ -1,5 +0,0 @@
-/home/fadi/split_bridge/target/release/deps/split_bridge-072717ae10f88dbe.d: src/main.rs
-
-/home/fadi/split_bridge/target/release/deps/split_bridge-072717ae10f88dbe: src/main.rs
-
-src/main.rs:
